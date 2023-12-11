6814aac06d51334a708cc73d6b7a172e13e4a2f4
dannyhannyford <dannyhannyford@gmail.com>
2023-12-10 23:42:16 -0600

    day2 pt1 complete

--- day2/day2.go
+++ day2/day2.go
@@ -3,10 +3,13 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log/slog"
 	"os"
+	"strconv"
+	"strings"
 )
 
-func main() {
+func main() {	
 	result, err := read_file("input.txt")
 	if err != nil {
 		fmt.Println(err)
@@ -23,7 +26,7 @@ func read_file(file_name string) (int, error) {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-	sum, err = read_lines_pt_1(scanner, sum)
+	sum, err = read_lines_pt_2(scanner, sum)
 	
 	if err := scanner.Err(); err != nil {
 		return 0, err
@@ -34,25 +37,81 @@ func read_file(file_name string) (int, error) {
 
 
 func read_lines_pt_1(scanner *bufio.Scanner, sum int) (int, error){
+	logger := slog.New(slog.NewTextHandler(os.Stderr, nil))
 	maxCubes := map[string]int{
 		"red": 12,
 		"green": 13,
 		"blue": 14,
 	}
+	
 	// sum of ID's of possible games
 	game_id := 1
 	for scanner.Scan() {
 		line := scanner.Text()
+		start_idx := strings.IndexByte(line, ':')
 		// start at :
-		// loop
-			// if two digit and >=12 check
-			// go until ; or end of line
-			// if not possible, continue
-
-		// add possible game_id to sum
-		
-
+		cube_substring := line[start_idx+1:]
+		cubeSlice := strings.Fields(cube_substring)
+		if validBag, err := isValidBag(cubeSlice, maxCubes, logger); err == nil {
+			// add possible game_id to sum
+			if validBag {
+				sum += game_id
+				logger.Info("success, add to sum", "game_id", game_id, "curr_sum", sum)
+			}
+		}
 		game_id++
 	}
+	logger.Info("sum", "sum", sum)
 	return sum, nil
+}
+
+func read_lines_pt_2(scanner *bufio.Scanner, sum int) (int, error) {
+	logger := slog.New(slog.NewTextHandler(os.Stderr, nil))
+	maxCubes := map[string]int{
+		"red": 12,
+		"green": 13,
+		"blue": 14,
+	}
+	minCubes := map[string]int{
+		"red": 0,
+		"green": 0,
+		"blue": 0,
+	}
+	
+	// sum of ID's of possible games
+	game_id := 1
+	for scanner.Scan() {
+		line := scanner.Text()
+		start_idx := strings.IndexByte(line, ':')
+		// start at :
+		cube_substring := line[start_idx+1:]
+		cubeSlice := strings.Fields(cube_substring)
+		if validBag, err := isValidBag(cubeSlice, maxCubes, logger); err == nil {
+			// add possible game_id to sum
+			if validBag {
+				sum += game_id
+				logger.Info("success, add to sum", "game_id", game_id, "curr_sum", sum)
+			}
+		}
+		game_id++
+	}
+	logger.Info("sum", "sum", sum)
+	return sum, nil
+}
+
+func isValidBag(cubeSlice []string, maxCubes map[string]int, logger *slog.Logger) (bool, error) {
+	for i := 0; i < len(cubeSlice); i++ {
+		if num, err := strconv.Atoi(cubeSlice[i]); err == nil {
+			color := cubeSlice[i+1]
+			if color[len(color)-1] == ';' || color[len(color)-1] == ',' {
+				color = color[0:len(color)-1]
+			}
+			if num > maxCubes[color] {
+				logger.Info("too many cubes", "color", color, "maxCubes", maxCubes[color], "claimed_cubes", num)
+				return false, nil
+			}
+			i++
+		}
+	}
+	return true, nil
 }
---
+++ day2/day2.md
@@ -0,0 +1,25 @@
+# Notes 
+strings.Indexbyte is for single byte characters
+faster than strings.Index
+
+for loop even faster
+
+# Strings
+read in bytes, need to be careful of multi-byte characters, use Runes if outside normal english
+
+%T = type
+%v = value
+
+x := 10
+fmt.Printf("%T, %v, x, x)
+
+Output:
+int, 10
+
+strconv.Atoi is short for strconv.ParseInt(s,10,0)
+
+`text/template package`
+Template strings
+
+# structured logging
+log/slog
